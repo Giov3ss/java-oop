921988cdcd84dc16e8a71011f9a9950893af4bde
Giovani Fonseca <112728772+Giov3ss@users.noreply.github.com>
2023-10-08 13:56:56 +0100

    adicionando o conceito de abstracao

---
+++ src/abstracao/ServicoMensagemInstantanea.java
@@ -0,0 +1,10 @@
+//a classe MSNMessenger é ou representa
+public abstract class ServicoMensagemInstantanea {
+	public abstract void enviarMensagem();
+	public abstract void receberMensagem();	
+}
+
+/**
+ * Em Java, o conceito de abstração é representado pela palavra reservada 
+ * **abstract**e métodos que NÃO possuem corpo na classe abstrata (pai).
+ */
